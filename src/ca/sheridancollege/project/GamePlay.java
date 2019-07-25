0f24dbbcfc2a4ea87402800d6115342deae18de8
EthoKurza <36766454+EthoKurza@users.noreply.github.com>
2019-07-25 11:46:21 -0400

    Base Skeleton for uno game

---
+++ src/ca/sheridancollege/project/GamePlay.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.project;
+
+import java.util.*;
+
+/**
+ *
+ * @author EthoKurza
+ */
+public class GamePlay {
+    
+    public static void main(String[] args){
+    
+    //Declare variables
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.println("Enter number of players: ");
+    int numPlayers = sc.nextInt();                  //get number of players
+    
+    //check if there are a minimum of 2 players playing
+    if(numPlayers < 2){
+        System.out.println("Error minimum of 2 players are needed to start the game.");
+        System.exit(0);                             //terminate program
+    }
+    Game uno = new Game(uno);
+    /* 
+    Player players[] = new Player[numPlayers];      //initialize player array
+    for(int i=0; i < players.length; i++){
+        System.out.printf("Enter player %d name: ",i+1);
+        String pName = sc.next();                   //get player's name
+        players[i].setPlayerID(pName);
+        
+        
+        //generate cards for each player
+        
+    }//end of for loop
+    */
+    
+    
+    //Start game
+        
+        
+        
+        //let each player play
+            //check player's cards
+            
+            //determine what the player wants to do (Eg. place/pick up card)
+            
+            //check if player has 0 cards in their hand
+                //end game and declare player as winner
+            
+    
+    
+    }//end of main method/function
+}//end of class
